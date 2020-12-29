48117ea8a7e357c410cbd339ff46197ffa515a82
Michiel Klaassen <michiel_m@live.nl>
2020-12-29 09:29:52 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # parse-ingredients
-Parse strings of ingredients to their name, unit, quantity and optional comments
+Parse strings of ingredients to their name, unit, quantity and optional comments.
+
+## To install
+```
+pip install parse-ingredients
+```
+
+## To use
+```python
+from recipe_searchers import search_recipe
+
+result = search_recipe("12 ounces lean ground beef, preferably 85 percent lean")
+print(f"Found results: \n {result}")
+```
+```search_recipe()``` returns an object in the form of:
+
+
+```json
+{
+    "name": "lean ground beef",
+    "quantity": 12,
+    "unit": "oz",
+    "comment": "preferably 85 percent lean",
+    "original_string": "12 ounces lean ground beef, preferably 85 percent lean"
+}
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parse-ingredients", # Replace with your own username
+    version="0.0.1",
+    author="Michiel K",
+    author_email="michieL_m@live.nl",
+    description="Parse strings of ingredients to their name, unit, quantity and optional comments.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MichielMag/parse-ingredients",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.6',
+)
